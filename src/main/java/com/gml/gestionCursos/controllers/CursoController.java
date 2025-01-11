3cadfa11cf04d3468608de5e608d3b16e5282a05
Bruzzone Marcelo Daniel <bruzzone1804@gmail.com>
2025-01-10 21:58:00 -0300

    listo

--- src/main/java/com/gml/gestionCursos/controllers/CursoController.java
+++ src/main/java/com/gml/gestionCursos/controllers/CursoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,6 +33,8 @@ public class CursoController {
     @GetMapping("/cursos/nuevo")
     public String agregarCurso(Model model) {
     	Curso curso = new Curso();
+
+        curso.setPublicado(true);
     	
     	model.addAttribute("curso", curso);
     	model.addAttribute("pageTitle", "Agregar Curso");
@@ -46,6 +49,32 @@ public class CursoController {
         }
         catch(Exception e){
             redirectAttributes.addAttribute("message", e.getMessage());
+        }
+        return "redirect:/cursos";
+    }
+
+    @GetMapping("/cursos/editar/{id}")
+    public String editarCurso(@PathVariable Long id,Model model,RedirectAttributes redirectAttributes){
+        try{
+            Curso curso = cursoRepository.findById(id).get();
+
+            model.addAttribute("curso",curso);
+            model.addAttribute("pageTitle","Editar curso : " + id);
+
+            return "agregarCurso";
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("message",e.getMessage());
+            return "redirect:/cursos";
+        }
+    }
+
+    @GetMapping("/cursos/delete/{id}")
+    public String eliminarCurso(@PathVariable Long id,Model model,RedirectAttributes redirectAttributes){
+        try{
+            cursoRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("message","El curso ha sido eliminado");
+        }catch (Exception exception){
+            redirectAttributes.addFlashAttribute("message",exception.getMessage());
         }
         return "redirect:/cursos";
     }
